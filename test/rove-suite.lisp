4b960b4b0ac147afc2b5b776e9430420d648d59c
Thomas HOULLIER <a>
2021-08-14 13:51:11 +0200

    added 2nd test

--- test/rove-suite.lisp
+++ test/rove-suite.lisp
@@ -5,9 +5,14 @@
 (deftest lup-decomp
   (testing "gauss-partial"
     (let ((a #2A((2 0 2 3/5) (3 3 4 -2) (5 5 4 2) (-1 -2 17/5 -1)))
+          (a2 #2A((1 2 0) (3 4 4) (5 6 3)))
+          (valid-lu2 #2A((5 6 3) (1/5 4/5 -3/5) (3/5 1/2 5/2)))
           (valid-lu #2A((5 5 4 2) (2/5 -2 2/5 -1/5)
                         (-1/5 1/2 4 -1/2) (3/5 0 2/5 -3)))
           (valid-p #(2 0 3 1))
-          (lu) (p))
+          (valid-p2 #(2 0 1))
+          (lu) (p) (lu2) (p2))
       (multiple-value-setq (lu p) (lup-decomp:gauss-partial a))
-      (ok (and (equalp valid-lu lu) (equalp valid-p p)) ""))))
+      (multiple-value-setq (lu2 p2) (lup-decomp:gauss-partial a2))
+      (ok (and (equalp valid-lu lu) (equalp valid-p p)) "Test #1")
+      (ok (and (equalp valid-lu2 lu2) (equalp valid-p2 p2)) "Test #2"))))
